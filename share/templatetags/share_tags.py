9f4f43fd28c7f45b88d7c069eca75c5c9e72cea6
Simone Deponti <simone.deponti@gmail.com>
2014-05-23 22:46:44 +0200

    Fixed several bugs.

--- share/templatetags/share_tags.py
+++ share/templatetags/share_tags.py
@@ -1,34 +1,54 @@
-import urlparse
-from django.template import Library, TemplateSyntaxError, Node
-from django.utils.http import urlquote
+from django.template import Library
+from django.template import Node
 from django.conf import settings
-from share.settings import *
+from share.settings import SHARE_PROVIDERS
 from django.template.loader import render_to_string
+
 
 register = Library()
 
+
 @register.simple_tag
 def share_css():
-	return "<link href='" + settings.STATIC_URL +"css/share.css' type='text/css' rel='stylesheet' />"
+    return "<link href='" + \
+        settings.STATIC_URL + \
+        "css/share.css' type='text/css' rel='stylesheet' />"
+
 
 @register.simple_tag
 def share_js():
-	return "<script src='" + settings.STATIC_URL +"js/share.js' type='text/javascript'></script>"
+    return "<script src='" + \
+        settings.STATIC_URL + \
+        "js/share.js' type='text/javascript'></script>"
+
 
 class ShareNode(Node):
-  def __init__(self, providers=None):
-    self.providers = providers
-  def render(self, context):
-    return render_to_string('share/links.html', {'providers': self.providers, 'url': context['request'].build_absolute_uri()}, context_instance=context)
+
+    def __init__(self, providers=None):
+        self.providers = providers
+
+    def render(self, context):
+        return render_to_string(
+            'share/links.html',
+            {
+                'providers': self.providers,
+                'url': context['request'].build_absolute_uri()
+            },
+            context_instance=context
+        )
+
 
 @register.tag
-def share(parser, token):
-  args = token.split_contents()
+def share(parser, token): # pylint: disable=unused-argument
+    args = token.split_contents()
 
-  if len(args) == 1:
-    providers = SHARE_PROVIDERS
-  else:
-    args.pop(0)
-    providers = args
+    if len(args) == 1:
+        providers = SHARE_PROVIDERS
+    else:
+        args.pop(0)
+        providers = {
+            'main': args,
+            'more': []
+        }
 
-  return ShareNode(providers)
+    return ShareNode(providers)
