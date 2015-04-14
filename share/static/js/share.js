7acad29e253820e6dbc72980fbe832cd588e47b9
Simone Deponti <simone.deponti@gmail.com>
2015-04-14 17:23:12 +0200

    Using HTTPS everywhere.

--- share/static/js/share.js
+++ share/static/js/share.js
@@ -26,30 +26,30 @@ $(function() {
 
         var opts = $.extend({}, $.fn.share.defaults, options);
         var loaded = new Array();
-        var url = document.location;
+        var url = window.encodeURIComponent(document.location);
 
         return this.each(function() {
-
+            var url;
             // Twitter
             if(opts.provider == "twitter") {
                 $(this).replaceWith('<a class="share-twitter-button button" href="https://twitter.com/share?url='+ url +'" target="_blank"></a><div class="share-twitter-count count">0</div>');
-                $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url='+url+'&callback=?', function(data) {
+                $.getJSON('https://urls.api.twitter.com/1/urls/count.json?url='+url+'&callback=?', function(data) {
                     $('.share-link-twitter .count').text(data.count);
                 });
             }
 
             // Facebook
             if(opts.provider == "facebook") {
-                $(this).replaceWith('<a class="share-facebook-button button" href="http://www.facebook.com/share.php?u='+ url +'" target="_blank"></a><div class="share-facebook-count count">0</div>');
-                $.getJSON('http://graph.facebook.com/'+url+'&callback=?', function(data) {
+                $(this).replaceWith('<a class="share-facebook-button button" href="https://www.facebook.com/share.php?u='+ url +'" target="_blank"></a><div class="share-facebook-count count">0</div>');
+                $.getJSON('https://graph.facebook.com/'+url+'&callback=?', function(data) {
                     $('.share-facebook-count').text(data.shares);
                 });
             }
 
             // Pinterest
             if(opts.provider == "pinterest") {
-                $(this).replaceWith('<a class="share-pinterest-button button" href="http://pinterest.com/pin/create/button/?url='+ url +'" target="_blank"></a><div class="share-pinterest-count count">0</div>');
-                $.getJSON('http://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=' + url, function(data) {
+                $(this).replaceWith('<a class="share-pinterest-button button" href="https://pinterest.com/pin/create/button/?url='+ url +'" target="_blank"></a><div class="share-pinterest-count count">0</div>');
+                $.getJSON('https://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=' + url, function(data) {
                     $('.share-pinterest-count').text(data.shares);
                 });
             }
